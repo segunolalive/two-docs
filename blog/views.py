600d631aeda8e7674f35c8c75dcc188951f62b76
segunolalive <segunolive@gmail.com>
2016-01-09 11:03:24 +0100

    made changes to view to truncate blog posts. Still yet make to link to all posts work properly

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,13 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[:5].reverse
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
+#def all_posts(request):
+#	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse
+#	return render(request, 'blog/all_posts.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
